53a99e6551069f3f4f5ac964bf645e200a7ebcfc
alsam8 <67710367+alsam8@users.noreply.github.com>
2020-07-25 09:11:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,4 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
-<script src="/script.js" defer></script>
-<script src="https://alsam8.github.io/cooperative-zinc-hare/script.js defer />
+
